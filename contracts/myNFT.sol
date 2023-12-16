018320dcd47cd2cfe40cfee081ccd5f5811da946
hliosone <sstelcher@protonmail.com>
2023-12-16 12:03:25 +0100

    Requirements done, test phase coming

--- contracts/myNFT.sol
+++ contracts/myNFT.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-//import the standard implementation of ERC721
+//import the standard implementation of ERC721 and Ownable for access right
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-//import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 // creation of an ERC721 contract
@@ -56,12 +55,49 @@ function getChildrens(uint256 _tokenId) external view returns (uint256[] memory)
     return tokenMetadataBook[_tokenId].childrens;
 }
 
-function setMetadata(string memory _description, uint256 _tokenNumber) public {
+function setMetadata(string memory _description, uint256 _tokenNumber, uint256 _newParentId) external onlyOwner {
+    //Check if we want to change the description or not
     if (bytes(_description).length != 0){
         tokenMetadataBook[_tokenNumber].description = _description;
     }
+
+    //Check if the newParentId token exists and modify children string
+    if(tokenMetadataBook[_newParentId].tokenOwner != address(0) && tokenMetadataBook[_tokenNumber].parentId != _newParentId){
+        if(tokenMetadataBook[_tokenNumber].parentId != 0){
+            deleteChildrenRelation(_tokenNumber);
+        }
+        addChildrenRelation(_newParentId, _tokenNumber);
+
+    }
 }
 
-//function deleteChildren(uint256 _parentId, uint256 _childrenToken) internal {}
-//function addChildren(uint256 _childrenToken) internal {}
+    //Take a children as an argument and will be deleted in parent token children list
+    function deleteChildrenRelation(uint256 _childrenToken) internal {
+        TokenMetadata memory children = tokenMetadataBook[_childrenToken];
+        uint256[] storage childrensList = tokenMetadataBook[children.parentId].childrens;
+
+        if(childrensList.length != 0){
+            for (uint256 i = 0; i < childrensList.length; ++i){
+                if (childrensList[i] == _childrenToken){
+                    childrensList[i] = childrensList[childrensList.length - 1];
+                    childrensList.pop();
+                    break;
+                }
+            }
+        }
+    }
+
+    //Add a children to parent children list if it's not already there and change the parent id
+    function addChildrenRelation(uint256 _newParentId, uint256 _childrenToken) internal {
+        //TokenMetadata memory parent = tokenMetadataBook[_newParentId];
+        uint256[] storage childrensList = tokenMetadataBook[_newParentId].childrens;
+        
+        for (uint256 i = 0; i < childrensList.length; ++i){
+            if (childrensList[i] == _childrenToken){
+                break;
+            }
+        }
+        childrensList.push(_childrenToken);
+        tokenMetadataBook[_childrenToken].parentId = _newParentId;
+    }
 }
